6bac69e82d5b91907a32f64586d416d97fb3ccb8
Jeron Aldaron Lau <aldaronlau@gmail.com>
2023-07-07 16:16:42 -0500

    Reorder optimizations

--- hello/build.sh
+++ hello/build.sh
@@ -7,6 +7,6 @@ export CARGO_BUILD_RUSTFLAGS="\
 cargo build --target wasm32-unknown-unknown --release && \
     cp ../target/wasm32-unknown-unknown/release/hello.wasm hello.wasm && \
     wasm-snip hello.wasm --snip-rust-panicking-code -o hello.wasm && \
-    wasm-strip hello.wasm && \
-    wasm-opt hello.wasm -o hello.wasm -Os
+    wasm-opt hello.wasm -o hello.wasm -Os && \
+    wasm-strip hello.wasm
 ls -l hello.wasm | awk '{print $5}'
